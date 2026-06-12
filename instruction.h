069ae775d0745a824343a0a48563cbbe816c4b82
james emmanuel alberto <jamesemmanuelalberto@jamess-MacBook-Pro.local>
2026-06-12 17:24:59 +0800

    added FDE loop functions

--- instruction.h
+++ instruction.h
@@ -1,5 +1,29 @@
-#ifndef CYCLE_TABLE_HEADER
-#include CYCLE_TABLE_HEADER
+#ifndef GB_CYCLE_TABLE_HEADER
+#define GB_CYCLE_TABLE_HEADER
+#include <iostream>
+#include <stdexcept>
 //write instructions
+//FDE(fetch, decode, execute)
+extern uint16_t PC;
+extern uint8_t data[];
+uint8_t inline fetchCOD() {
+  return data[PC++];
+}
+//DE(decode, execute)
+uint8_t DE(uint8_t opcode) {
+  switch(opcode) {
+    case0x00:
+      return 0x04;
+    break;
+    default:
+      std::cerr<<"unknown/banned instruction:0x"<<opcode<<std::endl;
+      throw std::runtime_error("wrong instruction!");
+    break;
+  }
+}
+uint8_t FDE() {
+  uint8_t opcode = fetchCOD();
+  return DE(opcode);
+}
 
 #endif
